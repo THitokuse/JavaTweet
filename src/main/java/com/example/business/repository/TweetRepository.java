7bb7906e8361c5dd77b83bd26c0d54ec969c72f1
TaichiroHasegawa <hasegawa.hitokuse@gmail.com>
2018-12-16 21:03:23 +0900

    ページネーション化
    TweetRepository
    TweetController
    tweet#index画面の編集

--- src/main/java/com/example/business/repository/TweetRepository.java
+++ src/main/java/com/example/business/repository/TweetRepository.java
@@ -1,5 +1,9 @@
 package com.example.business.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +11,7 @@ import com.example.business.domain.Tweet;
 
 @Repository
 public interface TweetRepository extends JpaRepository<Tweet, Long> {
-
+	List<Tweet> findAllByOrderByIdDesc();
+	
+	Page<Tweet> findAllByOrderByIdDesc(Pageable pageable);
 }
