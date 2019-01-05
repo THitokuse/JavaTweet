afaf886cdea432bcf1a99f880e63334848010d95
TaichiroHasegawa <hasegawa.hitokuse@gmail.com>
2019-01-05 12:23:42 +0900

    tweet.javaと@OneToManyアソシエーションを実装
    ツイートテーブルと1対多の関係を実装
        @OneToMany(mappedBy="user")
        private List<Tweet> tweets;

--- src/main/java/com/example/business/domain/User.java
+++ src/main/java/com/example/business/domain/User.java
@@ -6,47 +6,60 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Transient;
+import javax.persistence.OneToMany;
+import java.util.List;
 
 @Entity
 @Table(name = "user")
 public class User {
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
-	private String username;
-	private String email;
-	private String password;
-	@Transient
-	private String passwordConfirmation;
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getPasswordConfirmation() {
-		return passwordConfirmation;
-	}
-	public void setPasswordConfirmation(String passwordConfirmation) {
-		this.passwordConfirmation = passwordConfirmation;
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    @Transient
+    private String passwordConfirmation;
+    @OneToMany(mappedBy="user")
+    private List<Tweet> tweets;
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getPasswordConfirmation() {
+        return passwordConfirmation;
+    }
+    public void setPasswordConfirmation(String passwordConfirmation) {
+        this.passwordConfirmation = passwordConfirmation;
+    }
+    
+    public String getEmail() {
+    	return email;
+    }
+    public void setEmail(String email) {
+    	this.email = email;
+    }
+    
+    public List<Tweet> getTweets(){
+    	return tweets;
+    }
+    
+    public void setTweets(List<Tweet> tweets) {
+    	this.tweets = tweets;
+    }
 }
